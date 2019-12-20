c7201520dd7dc0d091802e3c5c2c70935733bae2
Vitalii Plagov <vitalii.plagov@gmail.com>
2019-12-21 00:14:32 +0200

    Add Junit5 (#1)
    * Add Junit5
    
    * Try not to build feature branches
    
    * Remove temporary test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
-    application
 }
 
 repositories {
@@ -11,4 +10,9 @@ repositories {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.junit.jupiter:junit-jupiter:5.5.2")
+}
+
+tasks.withType<Test>{
+    useJUnitPlatform()
 }
---
+++ src/test/kotlin/io/plagov/Test.kt
@@ -0,0 +1,12 @@
+package io.plagov
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Test {
+
+    @Test
+    fun foo() {
+        Assertions.assertTrue(true)
+    }
+}
