9ad011de78920ba66212aa92652228cc822d1856
Vitalii Plagov <vitalii.plagov@gmail.com>
2019-12-24 01:00:38 +0200

    Add Cucumber JVM support
    * add `cucumber-jvm` dependencies
    * add required Gradle tasks for Cucumber with respective configuration
    * add smoke test scenario to check if all Cucumber dependencies and
    configurations work as expected
    * change GitHub Actions workflow to run `cucumber` task instead of
    `build`, since `build` doesn't run tests

---
+++ src/test/kotlin/io/plagov/CucumberRunner.kt
@@ -0,0 +1,10 @@
+package io.plagov
+
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import org.junit.runner.RunWith
+
+@RunWith(Cucumber::class)
+@CucumberOptions(plugin = ["pretty"])
+class CucumberRunner {
+}
---
+++ src/test/resources/test.feature
@@ -0,0 +1,6 @@
+Feature: Smoke test feature
+
+  Scenario: Smoke test scenario
+    Given user has 2 apples
+    When user gets 2 more apples
+    Then user has total of 4 apples
